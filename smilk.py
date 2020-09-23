f832ea87bb95d9c348441dc9271730ed09b5e183
exurd <alastercaslin@outlook.com>
2020-09-23 17:51:25 +0100

    added spacing feature
    also combined some conditions to optimize it

--- smilk.py
+++ smilk.py
@@ -18,20 +18,29 @@ parser = argparse.ArgumentParser(description='Adds milk to the end of the word. 
 parser.add_argument('--word', '-w', type=str,  help='The word itself. No spaces!', default='s')
 parser.add_argument('--secondary_word', '-sw', type=str,  help='Secondary word. Added after milk. Not used by default.', default='')
 parser.add_argument('--milk', '-m', type=str,  help='Milk. Default is milk.', default='milk')
+parser.add_argument('--spaced', '-s',  help='Adds spaces between the words.', action='store_true')
 parser.add_argument('--version', action='version', version='%(prog)s 0.1')
 args, unknown = parser.parse_known_args()
 
-if args.word == 'got': # easter egg code
+if args.word == 'got' and args.spaced == True: # easter egg code
     print("got milk? i'm thirsty")
 elif args.word == 'milk' and args.secondary_word == 'milk': # easter egg code
-    print("When you typed in the command, the word 'milkmilkmilk' came on the screen.")
+    if args.spaced == True:
+        print("When you typed in the command, the word 'milk milk milk' came on the screen.")
+    else:
+        print("When you typed in the command, the word 'milkmilkmilk' came on the screen.")
     print("However, without realizing it, you accidentially created a black hole, which sucked you and everything into it.")
     print("GAME OVER")
     print("YOUR SCORE: 42")
     print("Want to play again?")
-elif args.word == 'milk' and args.milk == 'milk': # easter egg code
-    print(args.word + args.milk + args.secondary_word + "? isn't that a bit nonsensical?")
-elif args.secondary_word == 'milk' and args.milk == 'milk': # easter egg code
-    print(args.word + args.milk + args.secondary_word + "? isn't that a bit nonsensical?")
+elif args.word == 'milk' and args.milk == 'milk' or args.secondary_word == 'milk' and args.milk == 'milk': # easter egg code
+    if args.spaced == True:
+        print(args.word, args.milk, args.secondary_word + "? isn't that a bit nonsensical?")
+    else:
+        print(args.word + args.milk + args.secondary_word + "? isn't that a bit nonsensical?")
 else:
-    print(args.word + args.milk + args.secondary_word) # the main line of code. this is it. there's nothing else.
+    if args.spaced == True:
+        print(args.word, args.milk, args.secondary_word)
+    else:
+        print(args.word + args.milk + args.secondary_word)
+    # this is it. there's nothing else.
